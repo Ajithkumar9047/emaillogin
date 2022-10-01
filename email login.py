591d59fa5fb520a334c6813dad14b2d39b21e476
Ajithkumar9047 <114790836+Ajithkumar9047@users.noreply.github.com>
2022-10-01 09:43:30 +0530

    Add files via upload

---
+++ email login.py
@@ -0,0 +1,66 @@
+import re
+
+regex="[a-z0-9]+[\.]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+def check(email):
+
+   if (re.search(regex,email)):
+      print("mail id is correct")
+   else:
+      print("enter mail id is wrong")
+
+def passwords(password):
+ if  (5 < (len(password)) > 18):
+   print("password length must be greater than 5 and less than 18")
+   if not re.search('[A-Z]', password):
+     print("password required atleast one upper case A-Z")
+     if not re.search("[a-z]", password):
+        print("password required atleast one lower case a-z")
+        if not re.search("[0-9]", password):
+         print("password required atleast one numeric case 0-9")
+         if re.search("[\s]", password):
+            print('there is no space between password')
+            if not re.search("[/_&%#@]", password):
+             print("password required atleat one special character")
+ else:
+  print(F" {password} is correct")
+ write()
+
+def write():
+  dic = {"username": email, "password": password}
+  f =  open("D:/doc/aji.txt", "x")
+  f.writelines(f" {dic} ")
+
+def forgot():
+ f2=input("choose forgotpassword or exit")
+ if re.findall("[\b,forgotpassword]",f2):
+    d=input("enter your verification id")
+    if d==email:
+     print(f"your password:{password} ")
+    else:
+        print("enter id is wrong")
+ else:
+   print("exit")
+def login(button):
+    if  re.search("[\b,login]",button):
+        login_id=input("enter your login id")
+        login_password=input("enter your password")
+        class Person:
+            def __init__(self, email, password):
+                self.email = email
+                self.password = password
+        p1 = Person(login_id,login_password)
+        if ((email==p1.email)&(p1.password==password)):
+            print("welcome to email")
+        else:
+          print("wrong id")
+          forgot()
+
+    else:
+     print("exit")
+if __name__=="__main__":
+ email=input("enter your email")
+ check(email)
+ password=input("enter your password")
+ passwords(password)
+button=input("enter login or exit")
+login(button)
