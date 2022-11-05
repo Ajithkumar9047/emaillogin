0a61d0741ef44bd25d9dce759908db56b7045ada
Ajithkumar9047 <114790836+Ajithkumar9047@users.noreply.github.com>
2022-11-05 18:04:45 +0530

    telephone directory
    This project main moto is create telephone diractory data stored into mongodb database and read ,update,create and delete the data using python

---
+++ telephone directory/telephone directory.py
@@ -0,0 +1,58 @@
+import pymongo
+def insert():
+    name = input("enter your name")
+    place = input("enter your place")
+    gender = input("enter your gender")
+    phone_number = input("enter your phone_number")
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = client["telephone"]
+    mycol = mydb["directory"]
+    mydict = {"name": f'{name}',
+            "place": f'{place}',
+            "gender": f'{gender}',
+            "phone_number": f'{phone_number}'
+               }
+    x = mycol.insert_one( mydict)
+def retrive():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = client["telephone"]
+    mycol = mydb["directory"]
+    for x in mycol.find():
+        print(x)
+def update():
+    old_index = input("enter your  old_index like name ,gender,place,mobile_number")
+    old_data=input("enter old data ")
+    update_index = input("enter your  index like name ,gender,place,mobile_number")
+    update_data=input("enter your data to update")
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = client["telephone"]
+    mycol = mydb["directory"]
+    old_querey={(f'{old_index}'):f'{old_data}'}
+    myquerey={"$set":{(f'{update_index}'):f'{update_data}'}}
+    mycol.update_one(old_querey,myquerey)
+    print(f"{update_data}has been updated successfully")
+def delete():
+    delete_index = input("enter your delete index like name ,gender,place")
+    delete_data = input("enter your data to delete")
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = client["telephone"]
+    mycol = mydb["directory"]
+    x=mycol.delete_one({f'{delete_index}':f'{delete_data}'})
+    for x in mycol.find():
+        print(x)
+    print(f"has been deleted successfully")
+print("WELCOME TO TELEPHONE DIRECTORY")
+print("enter 1 to insert data")
+print("enter 2 to retrive data")
+print("enter 3 to update data")
+print("enter 4 to delete data")
+option=int(input("enter here..."))
+if option==1:
+       insert()
+if option==2:
+    retrive()
+if option == 3:
+    update()
+if option == 4:
+    delete()
+
